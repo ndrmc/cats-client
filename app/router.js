adb15ad45c9c854cc1a5940a8f5205556938de98
Alexander Suraphel <alexsuraphel@gmail.com>
2016-10-26 15:02:40 +0300

    Implement GRN details page

--- app/router.js
+++ app/router.js
@@ -12,6 +12,7 @@ Router.map(function() {
     this.route('list');
     this.route('new');
     this.route('edit', { path: '/edit/:id' } );
+    this.route('show', { path: '/show/:id' } );
   });
   this.route('run');
 });
